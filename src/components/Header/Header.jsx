9da8fbea07f6274d1758de305ea108e1fb19c796
simanto789 <hazrat.ali789@gmail.com>
2023-03-31 03:41:00 +0600

    prepare header content

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import logo from '../../image/3135715.png';
+
+const Header = () => {
+    return (
+        <div>
+            <nav class="navbar bg-light">
+                <nav class="container-fluid d-flex justify-content-between">
+                    <a class="navbar-brand fs-3 fw-bold">Knowledge Cafe</a>
+                    <form class="d-flex" role="search">
+                       <img className="img-fluid p-2" style={{ width: '15%' }} src={logo} alt="" />
+                    </form>
+                </nav>
+            </nav>
+        </div>
+    );
+};
+export default Header;
