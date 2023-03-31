77114a3b7df80d96c1e4c58e0de9b553d48038af
simanto789 <hazrat.ali789@gmail.com>
2023-03-31 17:03:58 +0600

    add font awesome icon

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
-const SingleCard = ({knowledge}) => {
-    
+const SingleCard = ({ knowledge }) => {
+
     return (
         <div>
-            <div className="knowledge-card card text-start">
-                    <div className="knowledge-poster mb-3">
-                        <img src={knowledge.images} style={{ height: 340 , width: 740 }} alt="" />
-                    </div>
-                    <div className="author-container d-flex justify-content-between">
-                      <div className='author-container d-flex gap-2'>
-                            <div>
-                            <img src={knowledge.authorImage} style={{ height: 50 , width: 50 }} alt="" />
-                            </div>
-                            <div>
+            <div className="knowledge-card card text-start mb-4">
+                <div className="knowledge-poster mb-2 ">
+                    <img className='img-fluid d-block mx-auto my-auto' src={knowledge.images} style={{ height: 340, width: 740 }} alt="" />
+                </div>
+                <div className="author-container d-flex justify-content-between">
+                    <div className='author-container d-flex gap-2'>
+                        <div>
+                            <img src={knowledge.authorImage} style={{ height: 50, width: 50 }} alt="" />
+                        </div>
+                        <div>
                             <h5>{knowledge.author}</h5>
                             <p>march 14 (4 days ago)</p>
-                            </div>
-                            </div>
-                        <div>
-                            <p>5 mnt read</p>
                         </div>
-                     </div>
-                    <h3>{knowledge.title}</h3>
-                    <button>mark as read</button>
+                    </div>
+                    <div>
+                        <p>5 min read <span> <FontAwesomeIcon icon={faBookmark} /></span> 
+                        </p>
+                    </div>
                 </div>
+                <h3>{knowledge.title}</h3>
+                <button className='btn btn-success'>mark as read</button>
+            </div>
         </div>
     );
 };
